b1646b33c4a2309b821d022fd68893af3e8e50a7
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-12-19 23:11:06 +0300

    scripts deleted from root folder with course work implementation

--- course_work/indices.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX "order_departure_index" ON ordering USING BTREE(departure_point_id);
-CREATE INDEX "order_destination_index" ON ordering USING BTREE(destination_point_id);
-CREATE INDEX "order_status_index" ON ordering USING BTREE(status);
-CREATE INDEX "courier_rating_index" ON courier USING BTREE(rating);
