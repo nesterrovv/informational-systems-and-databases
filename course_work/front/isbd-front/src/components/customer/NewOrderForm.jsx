55de499eb043c3e683fdc48696abb3ca9852801c
ZloyEgor <elopatov74@outlook.com>
2022-12-22 11:39:50 +0300

    store goods as array in redux

--- course_work/front/isbd-front/src/components/customer/NewOrderForm.jsx
+++ course_work/front/isbd-front/src/components/customer/NewOrderForm.jsx
@@ -1,124 +1,115 @@
-import React from 'react';
-import * as events from "events";
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
-class NewOrderForm extends React.Component {
+function NewOrderForm() {
 
-    constructor(props) {
-        super(props);
+    const dispatch = useDispatch();
+    const goods = useSelector(state => state.goods.goods);
 
-        this.state = {
-            departure: '',
-            destination: '',
-            description: '',
-            goods: []
-        }
-    }
-
-    componentDidMount() {
-        this.getCities();
-    }
-
-    getCities() {
+    const [departure, setDeparture] = useState('');
+    const [destination, setDestination] = useState('');
+    const [description, setDescription] = useState('');
+    const getCities = () => {
         //TODO: make ajax to server, get json of cities, setState()
     }
 
-    handleDepartureChange = event => {
-        this.setState({
-            departure: event.target.value
-        })
+    const handleDepartureChange = event => {
+        setDeparture(event.target.value);
     }
-    handleDestinationChange = event => {
-        this.setState({
-            destination: event.target.value
-        })
+    const handleDestinationChange = event => {
+        // this.setState({
+        //     destination: event.target.value
+        // })
+        setDestination(event.target.value);
     }
-    handleDescriptionChange = event => {
-        this.setState({
-            description: event.target.value
-        })
-    }
-
-    handleAddGood = (event) => {
-        let goods = [...this.state.goods];
-        let newGood = {}
-        goods.push(newGood);
-        this.setState({goods: goods});
+    const handleDescriptionChange = event => {
+        // this.setState({
+        //     description: event.target.value
+        // })
+        setDescription(event.target.value);
     }
 
-    handleSubmit = event => {
+    const handleAddGood = (event) => {
+        // let goods = [...this.state.goods];
+        // let newGood = {}
+        // goods.push(newGood);
+        // this.setState({goods: goods});
+        dispatch({type: "ADD_GOOD"});
+
+        console.log(goods);
+    }
+
+    const handleSubmit = event => {
         console.log(this.state);
         event.preventDefault();
     }
 
-    renderGoods = () => {
-        const goodList = this.state.goods.map(
+    const renderGoods = () => {
+        const goodList = goods.map(
             good => (
                 <div></div>
             )
         )
     }
 
-    render() {
-        return (
-            <div className="content">
-                <form onSubmit={this.handleSubmit}
-                      className="mw-full">
+    return (
+        <div className="content">
+            <form onSubmit={handleSubmit}
+                  className="mw-full">
 
-                    <div className="form-row row-eq-spacing">
-                        <div className="col">
-                            <div className="form-group">
-                                <label htmlFor="departure" className="required">Departure:</label>
+                <div className="form-row row-eq-spacing">
+                    <div className="col">
+                        <div className="form-group">
+                            <label htmlFor="departure" className="required">Departure:</label>
 
-                                <select className="form-control" id="departure" required="required"
-                                        value={this.state.departure}
-                                        onChange={this.handleDepartureChange}>
-                                    <option value="" selected="selected" disabled="disabled">From city:
-                                    </option>
-                                    <option value="front-end">Capital Knot City</option>
-                                    <option value="back-end">South Knot City</option>
-                                    <option value="full-stack">West Knot City</option>
-                                    {/*    {getCities}*/}
-                                </select>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="form-group">
-                                <label htmlFor="destination" className="required">Destination</label>
-                                <select className="form-control" id="destination" required="required"
-                                        value={this.state.destination}
-                                        onChange={this.handleDestinationChange}>
-                                    <option value="" selected="selected" disabled="disabled">From city:
-                                    </option>
-                                    <option value="front-end">Capital Knot City</option>
-                                    <option value="back-end">South Knot City</option>
-                                    <option value="full-stack">West Knot City</option>
-                                    {/*    {getCities}*/}
-                                </select>
-                            </div>
+                            <select className="form-control" id="departure" required="required"
+                                    value={departure}
+                                    onChange={handleDepartureChange}>
+                                <option value="" selected="selected" disabled="disabled">From city:
+                                </option>
+                                <option value="front-end">Capital Knot City</option>
+                                <option value="back-end">South Knot City</option>
+                                <option value="full-stack">West Knot City</option>
+                                {/*    {getCities}*/}
+                            </select>
                         </div>
                     </div>
+                    <div className="col">
+                        <div className="form-group">
+                            <label htmlFor="destination" className="required">Destination</label>
+                            <select className="form-control" id="destination" required="required"
+                                    value={destination}
+                                    onChange={handleDestinationChange}>
+                                <option value="" selected="selected" disabled="disabled">From city:
+                                </option>
+                                <option value="front-end">Capital Knot City</option>
+                                <option value="back-end">South Knot City</option>
+                                <option value="full-stack">West Knot City</option>
+                                {/*    {getCities}*/}
+                            </select>
+                        </div>
+                    </div>
+                </div>
 
-                    <div className="form-group">
-                        <label htmlFor="description">Description</label>
-                        <textarea className="form-control" id="description"
-                                  placeholder="Write a short description about your order"
-                                  value={this.state.description}
-                                  onChange={this.handleDescriptionChange}>
+                <div className="form-group">
+                    <label htmlFor="description">Description</label>
+                    <textarea className="form-control" id="description"
+                              placeholder="Write a short description about your order"
+                              value={description}
+                              onChange={handleDescriptionChange}>
                         </textarea>
-                    </div>
+                </div>
 
-                    <div className="content bg-dark p-20 m-0 mb-20">
+                <div className="content bg-dark p-20 m-0 mb-20">
 
-                        <button className="btn btn-primary" type="button" onClick={this.handleAddGood}>Add good</button>
-                    </div>
+                    <button className="btn btn-primary" type="button" onClick={handleAddGood}>Add good</button>
+                </div>
 
 
-                    <input className="btn btn-primary" type="submit" value="Submit"/>
-                </form>
-            </div>
-        );
-    }
-
+                <input className="btn btn-primary" type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
 }
 
 export default NewOrderForm;
--- course_work/front/isbd-front/src/store/goodReducer.js
+++ course_work/front/isbd-front/src/store/goodReducer.js
@@ -1,11 +1,22 @@
 const defaultState = {
+    nextId: 0,
     goods: []
 }
 
 export const goodReducer = (state = defaultState, action) => {
     switch (action.type) {
         case "ADD_GOOD":
-            // return
+            let newGoods = state.goods;
+            let good = {
+                id: state.nextId,
+                length: 0,
+                width: 0,
+                height: 0,
+                weight: 0,
+                description: ''
+            }
+            newGoods.push(good);
+            return {...state, goods: newGoods, nextId: state.nextId + 1};
         default:
             return state;
     }
