ab4acb43e84ed700304eb5c48264b33a79171644
Ivan Nesterov <nesterov.iv2018@gmail.com>
2023-01-06 14:22:26 +0700

    added default sprint boot runner

---
+++ course_work/isdb-backend/src/main/java/com/nesterrovv/isdbbackend/application/IsdbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nesterrovv.isdbbackend.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsdbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsdbBackendApplication.class, args);
+	}
+
+}
