9de8f93077f3391f4a6296f6c1257a0658126fcc
Ivan Nesterov <nesterov.iv2018@gmail.com>
2023-01-12 02:32:27 +0700

    added no-args and all-args constructors for order entity

--- course_work/isdb-backend/src/main/java/com/nesterrovv/isdbbackend/data/Ordering.java
+++ course_work/isdb-backend/src/main/java/com/nesterrovv/isdbbackend/data/Ordering.java
@@ -1,8 +1,12 @@
 package com.nesterrovv.isdbbackend.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ordering {
 
     private int order_id;
